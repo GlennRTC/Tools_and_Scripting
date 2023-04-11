d98daf5cfca41aaa36c103c42aa7a1a310e8cee3
GlennRTC <glenn.r.tomassi@gmail.com>
2023-04-11 08:40:10 -0500

    Añade comentarios. Script probado y funcional. Datos de sesion anonimizados.

--- SFTPClient.ps1
+++ SFTPClient.ps1
@@ -1,33 +1,44 @@
-# Configura Variables de Acceso
-$user = "username"
-$password = "password"
-$host = "example.com"
-$remotePath = "/Ordenes/"
-$localPath = "D:\OMNILAB\DERIVACIONES\PETICIONES\*.txt"
-
-# Carga modulo Assembly y abre 
+# Carga WinSCP .NET assembly
 Add-Type -Path "WinSCPnet.dll"
+
+
+# Configuracion de sesion y accesos (Estas credenciales son generadas usando el generador de script de sesion de WinSCP para PowerShell)
 $sessionOptions = New-Object WinSCP.SessionOptions -Property @{
     Protocol = [WinSCP.Protocol]::Sftp
-    HostName = $host
-    UserName = $user
-    Password = $password
+    HostName = "xxxxx.com.ar"
+    UserName = "xxxx"
+    Password = "xxxx"
+    SshHostKeyFingerprint = "ssh-rsa 2048 xxxxxxxxxxxxxxxxxxxxxxxxx"
 }
 
-# Conecta secion y transfiere archivo
 $session = New-Object WinSCP.Session
+
 try
 {
-    $session.Open($sessionOptions)
-    $transferResult = $session.PutFiles($localPath, $remotePath)
-    $transferResult.Check()
+    # Conexion a Sesion
+        $session.Open($sessionOptions)
+
+        # Carga de Ordenes
+        $transferOptions = New-Object WinSCP.TransferOptions
+        $transferOptions.TransferMode = [WinSCP.TransferMode]::Binary
+
+        $transferResult =
+            $session.PutFiles("D:\DERIVACIONES\PETICIONES\*.TXT", "/Ordenes/", $False, $transferOptions)
+ 
+
+        # Manejo de Errores
+        $transferResult.Check()
+
+ 
+
+        # Log de resultados del envio
+        foreach ($transfer in $transferResult.Transfers)
+        {
+            Write-Host "Upload of $($transfer.FileName) succeeded"
+        }
 }
 
-# Cierra secion.
 finally
 {
     $session.Dispose()
 }
-
-# Salida PowerShell
-Exit
