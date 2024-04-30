f689903e121394a199647e929cb8bf4038925401
Glenn R. Tomassi <glenn.r.tomassi@gmail.com>
2024-04-29 19:16:22 -0500

    Update MonitorTWMTRKmemory.ps1

--- MonitorTWMTRKmemory.ps1
+++ MonitorTWMTRKmemory.ps1
@@ -1,5 +1,6 @@
 $serviceName = "Omnilab_Analyser_Manager_TWM_TRK"
 $timestamp = Get-Date -Format "yyyyMMddHHmmss"
+$LogPath = "C:\Users\Administrator\Desktop\TWM_TRK_MemoryMonitoring"
 
 $process = Get-CimInstance Win32_Service | Where-Object { $_.Name -eq $serviceName } | ForEach-Object {
     Get-Process -Id $_.ProcessId
@@ -14,7 +15,7 @@ if ($process) {
         MemoryUsedMB = $memoryUsedMB
     }
 
-    $result | Export-Csv -Path "C:\Users\Administrator\Desktop\TWM_TRK_MemoryMonitoring\MemoryUsage-$serviceName-$timestamp.csv" -NoTypeInformation -Append
+    $result | Export-Csv -Path "$LogPath\MemoryUsage-$serviceName.csv" -NoTypeInformation -Append
     Write-Output "Memory usage for $serviceName written to 'MemoryUsage.csv' with timestamp."
 } else {
     Write-Output "No process found for service: $serviceName"
